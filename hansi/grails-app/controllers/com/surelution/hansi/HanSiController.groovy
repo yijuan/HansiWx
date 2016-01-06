d2335acc3884eaca4e96cb9e5f778fe3dec43a2c
Sunshineuun <583853240@qq.com>
2016-01-06 14:20:47 +0800

    2016/1/6

---
+++ hansi/grails-app/controllers/com/surelution/hansi/HanSiController.groovy
@@ -0,0 +1,51 @@
+package com.surelution.hansi
+
+class HanSiController {
+
+    def index() { }
+	
+	def intro(){
+		//简介
+	}
+	
+	def serve(){
+		//服务
+	}
+	
+	def business(){
+		//商业合作
+	}
+	
+	def product(){
+		//产品介绍
+	}
+	
+	def join(){
+		//加入我们
+	}
+	
+	def contact(){
+		//联系我们
+	}
+	
+	def securitySys(){
+		//安防系统
+	}
+	
+	def clockingInSys(){
+		//考勤系统
+	}
+	
+	def analysisSys(){
+		//损耗分析管理系统
+	}
+	
+	def bigDateSys(){
+		//大数据分析系统
+	}
+	
+	
+	def productIntro(){
+		//产品介绍
+	}
+}
